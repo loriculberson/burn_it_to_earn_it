fd5db4593d367360388ab939b79e1b3fd661782b
Lori Culberson <bigskybandit@sbcglobal.net>
2015-04-30 13:21:38 -0600

    WIP on adding dropdown

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,6 @@
 class Food < ActiveRecord::Base
   validates :name, :calories, presence: true, uniqueness: true
-  attr_reader :food_name
+  # attr_reader :food_name
 
   def self.service
     @service ||= FoodSearchService.new
@@ -9,7 +9,4 @@ class Food < ActiveRecord::Base
   def self.all(food_name)
     service.search_foods(food_name).values[0]
   end
-
-
-  
 end
