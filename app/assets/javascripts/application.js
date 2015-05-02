17c728e504d06cea30920356b11bf1f05947a188
Lori Culberson <bigskybandit@sbcglobal.net>
2015-05-02 13:14:14 -0600

    Typehead functions on food search box

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require bootstrap-sprockets
+//= require bootstrap3-typeahead
 //= require jquery_ujs
 //= require_tree .
 
--- app/assets/javascripts/food.js
+++ app/assets/javascripts/food.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
+  
+  $('#splurge').typeahead({
+    source: function(splurgeFood, processCallback){
+      $.ajax({
+          url: '/food_searches.json',
+          data: {food_search: splurgeFood},
+          success: function(results){
+            results.forEach(function(result){
+              result.name = result.table.name
+            })
+            processCallback(results);  
+          }
+      })
+    }
+  });
 
-
-    var earn_it = $('#earn-it-now');
+  /*
+    var earn_it = $('#find-food');
     earn_it.click(function(event) {
       event.preventDefault();
       var splurge_food = $('#splurge').val();
@@ -10,13 +25,14 @@ $(document).ready(function(){
         data: { food_search: splurge_food },
         success: function(results) {
           results.forEach(function(result) {
-            $("#results-list").append("<li class='nameOfClass for styling'>" + result.table.brand  + ': ' + result.table.name + '</li>');
+            $("#results-list").append("<li class='nameOfClass for styling'>" + result.table.brand  + " " + result.table.name + " " + result.table.calories +"</li>");
             
           });
           // list needs to populate a dropdown menu
+
           // a user selects an element from the dropdown and 
-          // upon selecting a food, the weight and submit box appears
-          // user enters his weight
+          // upon selecting a food, the weight box and submit button appears
+          // user enters his weight, clicks submit
           // $(earn_it).hide(); 
         }
       });
@@ -29,6 +45,10 @@ $(document).ready(function(){
     //     input.typeahead({ source: data });
     //     })
     // });
+*/
 })
+
+
+
 
 
