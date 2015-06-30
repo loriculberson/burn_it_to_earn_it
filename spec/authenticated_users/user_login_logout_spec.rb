155aaaff16cc655efd533ba09e35d21a03682fe5
Lori Culberson <bigskybandit@sbcglobal.net>
2015-06-30 02:13:39 -0500

    User can log out

--- spec/authenticated_users/user_login_logout_spec.rb
+++ spec/authenticated_users/user_login_logout_spec.rb
@@ -4,14 +4,25 @@ RSpec.describe "user login", type: :feature do
   before(:each) do
     OmniAuth.config.mock_auth[:twitter] = nil
   end
-    
-  it "can be able to log in with twitter" do
+
+  def user_login
     visit '/'
     expect(page).not_to have_content("Welcome twitter_mock_user")
     mock_omniauth_user
     click_on "Sign in with twitter"
+  end
+    
+  it "can be able to log in with twitter" do
+    user_login
 
     expect(page).to have_content("Welcome twitter_mock_user")
+  end
+
+  it "can logout" do
+    user_login
+    click_on 'Logout'
+    expect(page).to have_content("Successfully logged out!")
+    expect(page).not_to have_content("Welcome twitter_mock_user")
   end
 end
 
