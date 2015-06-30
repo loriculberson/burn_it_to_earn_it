36d991a92ab246ca2fee9669bcecb526254c1c35
Lori Culberson <bigskybandit@sbcglobal.net>
2015-06-30 14:18:20 -0500

    Change workout create params to correctly grab exercise id

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,12 +1,16 @@
 class WorkoutsController < ApplicationController
 
   def index
+    #check to see if this is getting hit. These are workout choices
     @workouts = current_user.workouts
   end
 
   def create
     if current_user 
-      workout = Workout.create(workout_params.merge(user_id: current_user.id, exercise_id: params["exercise_id"]))
+      workout = Workout.create(workout_params.merge(user_id: current_user.id, exercise_id: params["workout"]["exercise_id"]))
+      
+      workout.save!
+      
       redirect_to user_path
       flash[:notice] = "Workout saved!"
     else 
